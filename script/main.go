f4b656359e80261ab5acdf2f7d62c715c7e41333
Gaya <rrgaya@gmail.com>
2023-07-03 22:04:24 -0300

    Alteração no script para mudar user agente no local

--- script/main.go
+++ script/main.go
@@ -4,20 +4,37 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/rrgaya/ceifador/pkg/agentfy"
 )
 
-func main() {
-	uri := "https://apiconversion-od4zxa4f4a-rj.a.run.app/santander?aff_click_id=KWAI-MOBILE-G-1"
+func MakeRequest(urlSrc string) int {
+	client := &http.Client{}
 
-	for i := 0; i < 180; i++ {
-		response, err := http.Get(uri)
-		if err != nil {
-			log.Fatal(err)
-		}
+	req, err := http.NewRequest("GET", urlSrc, nil)
+	if err != nil {
+		log.Println("### SCRIPT  ### >>> Erro ao criar req:", err)
+	}
+
+	userAgent := agentfy.GenerateAgentfy()
+	req.Header.Set("User-Agent", userAgent)
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("### SCRIPT ### >>> Erro ao fazer DO da REQUEST:", err)
+	}
+	defer resp.Body.Close()
+	return resp.StatusCode
+}
+func main() {
+	// uri := "https://apiconversion-od4zxa4f4a-rj.a.run.app/santander?aff_click_id=KWAI-MOBILE-G-1"
+	uri := "https://grupodenegocios.com.br/versao6"
+	for i := 0; i < 10; i++ {
+		statusCode := MakeRequest(uri)
 
 		time.Sleep(time.Millisecond * 200)
 
-		log.Printf("Status Code: %d - Conversion: %d", response.StatusCode, i)
+		log.Printf("Status Code: %d - Conversion: %d", statusCode, i)
 	}
 
 }
